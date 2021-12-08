01ae308fdd88d38194a047e2a889ebfadf30acd4
Aleksei Ossipov <ossipovaleksei@gmail.com>
2021-12-08 18:57:07 +0200

    secondExercise commit

---
+++ src/Lesson2/secondExercise.java
@@ -0,0 +1,35 @@
+package Lesson2;
+
+public class secondExercise {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 32;
+        short secondNumber = 3255;
+        float thirdNumber = 4324634;
+        long fourthNumber = 4364563474356783L;
+        double fifthNumber = 214527;
+        byte sixthNumber;
+        int multiply;
+        long eightNumber;
+        double ninthNumber;
+        double tenNumber;
+
+        int plus = firstNumber + secondNumber;
+        long minus = (long) (fourthNumber - thirdNumber);
+        multiply =  firstNumber * secondNumber;
+        tenNumber = 45;
+        short devide = (short) (secondNumber / tenNumber);
+        eightNumber = 23265326L;
+        long devide2 = fourthNumber % eightNumber;
+
+        int number = 323;
+        int number2 = ++number;
+        int number3 = number2++;
+        int number4 = 544224;
+        int number5 = --number;
+        int number6 = number4--;
+
+    }
+
+}
